c1721c56a0692fc1595e960dc5f690000b6cfba1
khang <kvo0731@gmail.com>
2023-12-19 04:34:41 -0700

    Hover Effect
    Added a hover effect to fill on each div square when hovered over

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
-const container = document.querySelector('#container');
+const container = document.querySelector('#main-container');
 
 // Creates a 16x16 grid of square divs
 for(let i = 0; i < 16; i++) {
     for(let j = 0; j < 16; j++) {
         const square = document.createElement('div');
         square.classList.add('square');
+
+        square.addEventListener('mouseover', () => {
+            square.style.backgroundColor = '#ff0000';
+        });
+
         container.appendChild(square);
     }
 }
