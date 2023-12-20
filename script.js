60e996bbefa4fd2dda08fc30921bcdff3284417d
khang <kvo0731@gmail.com>
2023-12-19 22:16:19 -0700

    Finished Etch-A-Sketch
    Finalized the program

--- script.js
+++ script.js
@@ -1,15 +1,40 @@
-const container = document.querySelector('#main-container');
-
-// Creates a 16x16 grid of square divs
-for(let i = 0; i < 16; i++) {
-    for(let j = 0; j < 16; j++) {
-        const square = document.createElement('div');
-        square.classList.add('square');
-
-        square.addEventListener('mouseover', () => {
-            square.style.backgroundColor = '#ff0000';
-        });
-
-        container.appendChild(square);
+function promptUser() {
+    const squaresPerSide = prompt('Enter the number of squares per side (maximum 100): ')
+    
+    if (squaresPerSide && !isNaN(squaresPerSide) && squaresPerSide > 0 && squaresPerSide <= 100) {
+        createGrid(parseInt(squaresPerSide));
+    }
+    else {
+        alert('Invalid input. Please enter a number between 1 and 100.');
     }
 }
+
+function createGrid(squaresPerSide) {
+    const container = document.querySelector('#main-container');
+
+    while(container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+
+    // Creates a 16x16 grid of square divs
+    for(let i = 0; i < squaresPerSide; i++) {
+        for(let j = 0; j < squaresPerSide; j++) {
+            const square = document.createElement('div');
+            square.classList.add('square');
+
+            square.addEventListener('mouseover', () => {
+                square.style.backgroundColor = '#ff0000';
+            });
+
+            container.appendChild(square);
+        }
+    }
+
+    container.style.width = `${squaresPerSide * 16}px`;
+    
+}
+
+createGrid(16);
+
+const btn = document.querySelector('#popup-btn');
+btn.addEventListener('click', promptUser);
